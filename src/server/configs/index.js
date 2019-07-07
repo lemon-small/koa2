8d1c88a75f9dc5f01fde27ab92c817704dcf9b4d
lemon-small <jonathan@junliu-jonathandeMacBook-Air.local>
2019-07-07 19:00:23 +0800

    up

--- src/server/configs/index.js
+++ src/server/configs/index.js
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV == "dev") {
 
 if (process.env.NODE_ENV == "prod") {
     config = {
-        'ViewDir': path.join(__dirname, './'),
+        'ViewDir': path.join(__dirname, 'web'),
         'AssetsDir': path.join(__dirname, './')
     };
     const prodConfig = {
